bfd5f172dd275c0cb0627eb14c9f375bf063ef09
Ian Junio <ianchristopherjunio19@gmail.com>
2026-03-08 00:46:52 +0800

    Add Main class for JavaFX application

---
+++ codecafe/Main.java
@@ -0,0 +1,30 @@
+package codecafe;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/codecafe/view/start_screen.fxml"));
+        Parent root = loader.load();
+
+        // Create scene
+        Scene scene = new Scene(root, 1920, 1080);
+
+        // Set title and show stage
+        primaryStage.setTitle("Code Coffee");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+}
